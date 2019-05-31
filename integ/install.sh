ce5b98c4ca0948eca25a9a942c3b29fa8fce0aad
anandswaminathan <aswaminathan@lyft.com>
2019-05-31 00:09:25 -0700

    Change version and image

--- integ/install.sh
+++ integ/install.sh
@@ -4,7 +4,7 @@ set -e
 
 make docker_build
 
-sudo snap install microk8s --classic --edge
+sudo snap install microk8s --classic --channel=1.14/stable
 
 sh boilerplate/lyft/golang_test_targets/dep_install.sh
 
--- integ/setup.sh
+++ integ/setup.sh
@@ -2,7 +2,7 @@
 
 export NAMESPACE=default
 export OPERATOR_IMAGE=flinkk8soperator:$(git rev-parse HEAD)
-export IMAGE=localhost:32000/flinkk8soperator:local
+export IMAGE=127.0.0.1:32000/flinkk8soperator:local
 
 docker tag $OPERATOR_IMAGE $IMAGE
 
@@ -10,6 +10,6 @@ microk8s.start
 microk8s.status --wait-ready
 microk8s.enable dns registry
 
+docker push 127.0.0.1:32000/flinkk8soperator
 microk8s.kubectl proxy --port 8001 &
 microk8s.kubectl config view > ~/.kube/config
-docker push localhost:32000/flinkk8soperator
