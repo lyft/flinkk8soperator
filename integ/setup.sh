0be22b8b874f32758a7d0d542f5abceb59c2f07f
anandswaminathan <aswaminathan@lyft.com>
2019-05-31 02:02:49 -0700

    Retry until push

--- integ/setup.sh
+++ integ/setup.sh
@@ -7,9 +7,16 @@ export IMAGE=127.0.0.1:32000/flinkk8soperator:local
 docker tag $OPERATOR_IMAGE $IMAGE
 
 microk8s.start
-microk8s.status --wait-ready
 microk8s.enable dns registry
+microk8s.status --wait-ready
 
-docker push 127.0.0.1:32000/flinkk8soperator
+images=$(docker images 127.0.0.1:32000/flinkk8soperator -q | xargs)
+while [ "$images" == "" ]
+do
+  docker push 127.0.0.1:32000/flinkk8soperator
+  images=$(docker images 127.0.0.1:32000/flinkk8soperator -q | xargs)
+  sleep 5
+done
+
 microk8s.kubectl proxy --port 8001 &
 microk8s.kubectl config view > ~/.kube/config
