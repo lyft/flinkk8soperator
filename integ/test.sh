064b02ef9b6fb9eac3cb1b3ff23de2a44622ba07
anandswaminathan <aswaminathan@lyft.com>
2019-05-31 00:46:54 -0700

    Change version and image

--- integ/test.sh
+++ integ/test.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+microk8s.kubectl apply -f integ/abc.yaml
+watch -n 5 microk8s.kubectl get pods
+
 export INTEGRATION=true
 
 # needed to create the checkpoints directory with world-writable permissions
