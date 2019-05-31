4d915b53452639b7a1876341053fec4526215f1b
anandswaminathan <aswaminathan@lyft.com>
2019-05-30 22:08:38 -0700

    Change version

--- integ/setup.sh
+++ integ/setup.sh
@@ -2,14 +2,13 @@
 
 export NAMESPACE=default
 export OPERATOR_IMAGE=flinkk8soperator:$(git rev-parse HEAD)
-export IMAGE=localhost:32000/flinkk8soperator:registry
+export IMAGE=localhost:32000/flinkk8soperator:local
 
 docker tag $OPERATOR_IMAGE $IMAGE
 
 microk8s.start
 microk8s.status --wait-ready
-microk8s.enable dns
-microk8s.enable registry
+microk8s.enable dns registry
 
 microk8s.kubectl proxy --port 8001 &
 microk8s.kubectl config view > ~/.kube/config
