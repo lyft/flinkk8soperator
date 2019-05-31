9349a80987ac848a430a97e50de9c6dee33a8a28
anandswaminathan <aswaminathan@lyft.com>
2019-05-30 21:16:49 -0700

    Upgrade microk8s

--- integ/install.sh
+++ integ/install.sh
@@ -5,7 +5,7 @@ set -e
 make docker_build
 docker save flinkk8soperator > flinkk8soperator.tar
 
-sudo snap install microk8s --classic --channel=1.12/stable
+sudo snap install microk8s --classic --channel=1.14/stable
 
 sh boilerplate/lyft/golang_test_targets/dep_install.sh
 
--- integ/setup.sh
+++ integ/setup.sh
@@ -7,7 +7,7 @@ microk8s.start
 microk8s.status --wait-ready
 microk8s.enable dns
 
-microk8s.ctr -n $NAMESPACE flinkk8soperator import flinkk8soperator.tar
+microk8s.ctr -n $NAMESPACE image import flinkk8soperator.tar
 
 microk8s.kubectl proxy --port 8001 &
 microk8s.kubectl config view > ~/.kube/config
