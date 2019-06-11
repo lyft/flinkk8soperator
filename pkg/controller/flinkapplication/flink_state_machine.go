b2e9992a8da89f76e4ab743b7014383dc0b6ab08
Anand Swaminathan <aswaminathan@lyft.com>
2019-06-10 17:44:41 -0700

    Remove duplicate logging

--- pkg/controller/flinkapplication/flink_state_machine.go
+++ pkg/controller/flinkapplication/flink_state_machine.go
@@ -100,7 +100,6 @@ func (s *FlinkStateMachine) Handle(ctx context.Context, application *v1alpha1.Fl
 	currentPhase := application.Status.Phase
 	if _, ok := s.metrics.stateMachineHandlePhaseMap[currentPhase]; !ok {
 		errMsg := fmt.Sprintf("Invalid state %s for the application", currentPhase)
-		logger.Errorf(ctx, errMsg)
 		return errors.New(errMsg)
 	}
 	timer := s.metrics.stateMachineHandlePhaseMap[currentPhase].Start(ctx)
