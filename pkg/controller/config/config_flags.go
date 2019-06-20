d676d8c9c411d062d5d7f8799cd1b7e39a131906
Lakshmi Gururaja Rao <glaksh100@gmail.com>
2019-06-20 14:02:39 -0700

    Actually generate

--- pkg/controller/config/config_flags.go
+++ pkg/controller/config/config_flags.go
@@ -4,14 +4,42 @@
 package config
 
 import (
+	"encoding/json"
+	"reflect"
+
 	"fmt"
 
 	"github.com/spf13/pflag"
 )
 
+// If v is a pointer, it will get its element value or the zero value of the element type.
+// If v is not a pointer, it will return it as is.
+func (Config) elemValueOrNil(v interface{}) interface{} {
+	if t := reflect.TypeOf(v); t.Kind() == reflect.Ptr {
+		if reflect.ValueOf(v).IsNil() {
+			return reflect.Zero(t.Elem()).Interface()
+		} else {
+			return reflect.ValueOf(v).Interface()
+		}
+	} else if v == nil {
+		return reflect.Zero(t).Interface()
+	}
+
+	return v
+}
+
+func (Config) mustMarshalJSON(v json.Marshaler) string {
+	raw, err := v.MarshalJSON()
+	if err != nil {
+		panic(err)
+	}
+
+	return string(raw)
+}
+
 // GetPFlagSet will return strongly types pflags for all fields in Config and its nested types. The format of the
 // flags is json-name.json-sub-name... etc.
-func (Config) GetPFlagSet(prefix string) *pflag.FlagSet {
+func (cfg Config) GetPFlagSet(prefix string) *pflag.FlagSet {
 	cmdFlags := pflag.NewFlagSet("Config", pflag.ExitOnError)
 	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "resyncPeriod"), "30s", "Determines the resync period for all watchers.")
 	cmdFlags.String(fmt.Sprintf("%v%v", prefix, "limitNamespace"), "", "Namespaces to watch for by flink operator")
