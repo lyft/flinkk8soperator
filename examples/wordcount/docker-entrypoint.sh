c2158e7063c8c34231719926e7ff35ecf030f9f6
Thomas Weise <tweise@users.noreply.github.com>
2019-11-27 13:34:42 -0800

    Update wordcount example to use docker-flink image #135 (#136)

--- examples/wordcount/docker-entrypoint.sh
+++ examples/wordcount/docker-entrypoint.sh
@@ -1,45 +1,10 @@
 #!/bin/sh
 
-drop_privs_cmd() {
-    if [ $(id -u) != 0 ]; then
-        # Don't need to drop privs if EUID != 0
-        return
-    elif [ -x /sbin/su-exec ]; then
-        # Alpine
-        echo su-exec
-    else
-        # Others
-        echo gosu flink
-    fi
-}
-
-# Add in extra configs set by the operator
+# Map config from FlinkK8sOperator to base container
+# https://github.com/lyft/flinkk8soperator/issues/135
+# https://github.com/docker-flink/docker-flink/pull/91
 if [ -n "$OPERATOR_FLINK_CONFIG" ]; then
-    echo "$OPERATOR_FLINK_CONFIG" >> "/usr/local/flink-conf.yaml"
+    export FLINK_PROPERTIES="`echo \"${OPERATOR_FLINK_CONFIG}\" | envsubst`"
 fi
 
-envsubst < /usr/local/flink-conf.yaml > $FLINK_HOME/conf/flink-conf.yaml
-
-COMMAND=$@
-
-if [ $# -lt 1 ]; then
-    COMMAND="local"
-fi
-
-if [ "$COMMAND" = "help" ]; then
-    echo "Usage: $(basename "$0") (jobmanager|taskmanager|local|help)"
-    exit 0
-elif [ "$FLINK_DEPLOYMENT_TYPE" = "jobmanager" ]; then
-    echo "Starting Job Manager"
-    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
-    exec $(drop_privs_cmd) "$FLINK_HOME/bin/jobmanager.sh" start-foreground
-elif [ "$FLINK_DEPLOYMENT_TYPE" = "taskmanager" ]; then
-    echo "Starting Task Manager"
-    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
-    exec $(drop_privs_cmd) "$FLINK_HOME/bin/taskmanager.sh" start-foreground
-elif [ "$COMMAND" = "local" ]; then
-    echo "Starting local cluster"
-    exec $(drop_privs_cmd) "$FLINK_HOME/bin/jobmanager.sh" start-foreground local
-fi
-
-exec "$@"
+exec /docker-entrypoint.sh "$@"
