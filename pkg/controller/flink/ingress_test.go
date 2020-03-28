53dbc377717233991e9b5a3f079fc6070d9f9739
glaksh100 <lrao@lyft.com>
2020-03-27 17:02:25 -0700

    Fix ingress

--- pkg/controller/flink/ingress_test.go
+++ pkg/controller/flink/ingress_test.go
@@ -3,6 +3,8 @@ package flink
 import (
 	"testing"
 
+	"github.com/lyft/flinkk8soperator/pkg/apis/app/v1beta1"
+
 	config2 "github.com/lyft/flinkk8soperator/pkg/controller/config"
 	"github.com/stretchr/testify/assert"
 )
@@ -24,4 +26,17 @@ func TestGetFlinkUIIngressURL(t *testing.T) {
 	assert.Equal(t,
 		"ABC.lyft.xyz",
 		GetFlinkUIIngressURL("ABC"))
+}
+
+func TestGetFlinkUIIngressURLBlueGreenDeployment(t *testing.T) {
+	err := initTestConfigForIngress()
+	assert.Nil(t, err)
+	app := v1beta1.FlinkApplication{}
+	app.Spec.DeploymentMode = v1beta1.DeploymentModeBlueGreen
+	app.Name = "ABC"
+	app.Status.UpdatingVersion = v1beta1.GreenFlinkApplication
+	assert.Equal(t, "ABC-green", getIngressName(&app))
+	assert.Equal(t,
+		"ABC-green.lyft.xyz",
+		GetFlinkUIIngressURL(getIngressName(&app)))
 }
