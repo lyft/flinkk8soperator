521bac5a6992f45895ae138c1485d57962741788
Seth Saperstein <sethsaperstein@lyft.com>
2023-03-24 18:27:56 -0700

    disable rbac

--- integ/minikube_install.sh
+++ integ/minikube_install.sh
@@ -5,7 +5,7 @@ set -e
 curl -LO -s https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
-minikube start --kubernetes-version=v1.20.15
+minikube start --kubernetes-version=v1.20.15 --extra-config=apiserver.authorization-mode=AlwaysAllow
 
 sh boilerplate/lyft/golang_test_targets/dep_install.sh
 
