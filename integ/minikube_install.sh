15787c6bc028dbb327b006973452701938908873
Seth Saperstein <sethsaperstein@lyft.com>
2023-03-24 18:09:29 -0700

    fix minikube command

--- integ/minikube_install.sh
+++ integ/minikube_install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+curl -LO -s https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 minikube start --kubernetes-version=v1.20.15
--- integ/minikube_setup.sh
+++ integ/minikube_setup.sh
@@ -2,15 +2,15 @@
 
 # Test App Setup
 
-cd integ/operator-test-app
-export TEST_APP_IMAGE=operator-test-app:$(git rev-parse HEAD)
-docker build -t $TEST_APP_IMAGE .
-docker tag $TEST_APP_IMAGE flink-test-app:local.1
-docker tag $TEST_APP_IMAGE flink-test-app:local.2
-minikube image push flink-test-app:local.1
-minikube image push flink-test-app:local.2
-
-cd ../../
+#cd integ/operator-test-app
+#export TEST_APP_IMAGE=operator-test-app:$(git rev-parse HEAD)
+#docker build -t $TEST_APP_IMAGE .
+#docker tag $TEST_APP_IMAGE flink-test-app:local.1
+#docker tag $TEST_APP_IMAGE flink-test-app:local.2
+#minikube image load flink-test-app:local.1
+#minikube image load flink-test-app:local.2
+#
+#cd ../../
 
 # Operator Setup
 
@@ -19,6 +19,6 @@ export OPERATOR_IMAGE=flinkk8soperator:local
 
 docker build -t $DOCKER_IMAGE .
 docker tag $DOCKER_IMAGE $OPERATOR_IMAGE
-minikube image push $OPERATOR_IMAGE
+minikube image load $OPERATOR_IMAGE
 
 kubectl proxy --port 8001 &
