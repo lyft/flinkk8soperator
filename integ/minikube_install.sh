9ea26a86171310f082ddb07d915359fd04b71304
Seth Saperstein <sethsaperstein@lyft.com>
2023-03-26 18:39:40 -0700

    bump minikube mem

--- integ/minikube_install.sh
+++ integ/minikube_install.sh
@@ -5,6 +5,7 @@ set -e
 curl -LO -s https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
+minikube config set memory 7000
 minikube start --kubernetes-version=v1.20.15
 minikube ssh 'sudo cat /etc/kubernetes/manifests/kube-apiserver.yaml | sed -r "s/--authorization-mode=.+/--authorization-mode=AlwaysAllow/g" | sudo tee /etc/kubernetes/manifests/kube-apiserver.yaml'
 
