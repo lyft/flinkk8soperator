6e86f7035d870c81c57a61215a729e85eddf686c
Seth Saperstein <sethsaperstein@lyft.com>
2023-03-29 16:28:25 -0700

    check if kube dir exists

--- integ/install.sh
+++ integ/install.sh
@@ -8,9 +8,9 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube config set memory 6800
 minikube start --kubernetes-version=v1.20.15
 
-ls /etc/kubernetes/
-cat /etc/kubernetes/admin.conf
-cp /etc/kubernetes/admin.conf $HOME/.kube/config
+echo $HOME/.kube/config
+ls $HOME/.kube/
+cat $HOME/.kube/config
 
 sh boilerplate/lyft/golang_test_targets/dep_install.sh
 
