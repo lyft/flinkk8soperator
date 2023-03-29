3db9f0c0bd588aaf27f82e0711788235ca12118e
Seth Saperstein <sethsaperstein@lyft.com>
2023-03-29 16:26:02 -0700

    after cluster start

--- integ/install.sh
+++ integ/install.sh
@@ -4,12 +4,13 @@ set -e
 
 curl -LO -s https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
-ls /etc/kubernetes/
-cat /etc/kubernetes/admin.conf
-cp /etc/kubernetes/admin.conf $HOME/.kube/config
 
 minikube config set memory 6800
 minikube start --kubernetes-version=v1.20.15
+
+ls /etc/kubernetes/
+cat /etc/kubernetes/admin.conf
+cp /etc/kubernetes/admin.conf $HOME/.kube/config
 
 sh boilerplate/lyft/golang_test_targets/dep_install.sh
 
