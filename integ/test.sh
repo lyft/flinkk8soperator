db88cf908b19366446d241c58e7e1b2ab88cceb4
Seth Saperstein <sethsaperstein@lyft.com>
2023-03-30 05:58:03 -0700

    remove commented env var

--- integ/test.sh
+++ integ/test.sh
@@ -4,7 +4,6 @@ set -e
 
 export INTEGRATION=true
 export OPERATOR_IMAGE=flinkk8soperator:local
-# export KUBERNETES_CONFIG=/home/runner/.kube/config
 
 cd $(dirname "$0")
 go test -p 1 -timeout 40m -check.vv IntegSuite
