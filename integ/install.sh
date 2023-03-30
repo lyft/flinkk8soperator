69ccd27bd7057aa66eb5739528f38e5b11c065c2
Seth Saperstein <sethsaperstein@lyft.com>
2023-03-29 17:08:43 -0700

    move env var to test.sh since sudo

--- integ/install.sh
+++ integ/install.sh
@@ -8,8 +8,6 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube config set memory 6800
 minikube start --kubernetes-version=v1.20.15
 
-export KUBERNETES_CONFIG=/home/runner/.kube/config
-
 sh boilerplate/lyft/golang_test_targets/dep_install.sh
 
 dep ensure
