0c97c8f6bcde99423e22038325761323c89702cb
Leo Luo😺 <leoluo@lyft.com>
2023-04-04 11:53:09 -0700

    add more debug message to setup.sh

--- integ/setup.sh
+++ integ/setup.sh
@@ -5,9 +5,11 @@
 # TODO: upgrade flink test app from 1.8
 cd integ/operator-test-app
 export TEST_APP_IMAGE=operator-test-app:$(git rev-parse HEAD)
+echo "Building test app image locally"
 docker build -t $TEST_APP_IMAGE .
 docker tag $TEST_APP_IMAGE flink-test-app:local.1
 docker tag $TEST_APP_IMAGE flink-test-app:local.2
+echo "Pushing image to minikube"
 minikube image load flink-test-app:local.1
 minikube image load flink-test-app:local.2
 
@@ -23,7 +25,7 @@ cd ../../
 
 export DOCKER_IMAGE=flinkk8soperator:$(git rev-parse HEAD)
 export OPERATOR_IMAGE=flinkk8soperator:local
-
+echo "Building operator image locally"
 docker build -t $DOCKER_IMAGE .
 docker tag $DOCKER_IMAGE $OPERATOR_IMAGE
 minikube image load $OPERATOR_IMAGE
