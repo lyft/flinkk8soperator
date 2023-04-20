5c059aae2bf31bc0a82815e0996d9316f6ccb129
Leo Luo😺 <leoluo@lyft.com>
2023-04-20 14:07:53 -0700

    add more ctx

--- pkg/client/clientset/versioned/typed/app/v1beta2/fake/fake_flinkapplication.go
+++ pkg/client/clientset/versioned/typed/app/v1beta2/fake/fake_flinkapplication.go
@@ -3,6 +3,7 @@
 package fake
 
 import (
+	"context"
 	v1beta1 "github.com/lyft/flinkk8soperator/pkg/apis/app/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
@@ -23,7 +24,7 @@ var flinkapplicationsResource = schema.GroupVersionResource{Group: "flink.k8s.io
 var flinkapplicationsKind = schema.GroupVersionKind{Group: "flink.k8s.io", Version: "v1beta1", Kind: "FlinkApplication"}
 
 // Get takes name of the flinkApplication, and returns the corresponding flinkApplication object, and an error if there is any.
-func (c *FakeFlinkApplications) Get(name string, options v1.GetOptions) (result *v1beta1.FlinkApplication, err error) {
+func (c *FakeFlinkApplications) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1beta1.FlinkApplication, err error) {
 	obj, err := c.Fake.
 		Invokes(testing.NewGetAction(flinkapplicationsResource, c.ns, name), &v1beta1.FlinkApplication{})
 
@@ -34,7 +35,7 @@ func (c *FakeFlinkApplications) Get(name string, options v1.GetOptions) (result 
 }
 
 // List takes label and field selectors, and returns the list of FlinkApplications that match those selectors.
-func (c *FakeFlinkApplications) List(opts v1.ListOptions) (result *v1beta1.FlinkApplicationList, err error) {
+func (c *FakeFlinkApplications) List(ctx context.Context, opts v1.ListOptions) (result *v1beta1.FlinkApplicationList, err error) {
 	obj, err := c.Fake.
 		Invokes(testing.NewListAction(flinkapplicationsResource, flinkapplicationsKind, c.ns, opts), &v1beta1.FlinkApplicationList{})
 
@@ -56,14 +57,14 @@ func (c *FakeFlinkApplications) List(opts v1.ListOptions) (result *v1beta1.Flink
 }
 
 // Watch returns a watch.Interface that watches the requested flinkApplications.
-func (c *FakeFlinkApplications) Watch(opts v1.ListOptions) (watch.Interface, error) {
+func (c *FakeFlinkApplications) Watch(ctx context.Context, opts v1.ListOptions) (watch.Interface, error) {
 	return c.Fake.
 		InvokesWatch(testing.NewWatchAction(flinkapplicationsResource, c.ns, opts))
 
 }
 
 // Create takes the representation of a flinkApplication and creates it.  Returns the server's representation of the flinkApplication, and an error, if there is any.
-func (c *FakeFlinkApplications) Create(flinkApplication *v1beta1.FlinkApplication) (result *v1beta1.FlinkApplication, err error) {
+func (c *FakeFlinkApplications) Create(ctx context.Context, flinkApplication *v1beta1.FlinkApplication) (result *v1beta1.FlinkApplication, err error) {
 	obj, err := c.Fake.
 		Invokes(testing.NewCreateAction(flinkapplicationsResource, c.ns, flinkApplication), &v1beta1.FlinkApplication{})
 
@@ -74,7 +75,7 @@ func (c *FakeFlinkApplications) Create(flinkApplication *v1beta1.FlinkApplicatio
 }
 
 // Update takes the representation of a flinkApplication and updates it. Returns the server's representation of the flinkApplication, and an error, if there is any.
-func (c *FakeFlinkApplications) Update(flinkApplication *v1beta1.FlinkApplication) (result *v1beta1.FlinkApplication, err error) {
+func (c *FakeFlinkApplications) Update(ctx context.Context, flinkApplication *v1beta1.FlinkApplication) (result *v1beta1.FlinkApplication, err error) {
 	obj, err := c.Fake.
 		Invokes(testing.NewUpdateAction(flinkapplicationsResource, c.ns, flinkApplication), &v1beta1.FlinkApplication{})
 
@@ -85,7 +86,7 @@ func (c *FakeFlinkApplications) Update(flinkApplication *v1beta1.FlinkApplicatio
 }
 
 // Delete takes name of the flinkApplication and deletes it. Returns an error if one occurs.
-func (c *FakeFlinkApplications) Delete(name string, options *v1.DeleteOptions) error {
+func (c *FakeFlinkApplications) Delete(ctx context.Context, name string, options *v1.DeleteOptions) error {
 	_, err := c.Fake.
 		Invokes(testing.NewDeleteAction(flinkapplicationsResource, c.ns, name), &v1beta1.FlinkApplication{})
 
@@ -93,7 +94,7 @@ func (c *FakeFlinkApplications) Delete(name string, options *v1.DeleteOptions) e
 }
 
 // DeleteCollection deletes a collection of objects.
-func (c *FakeFlinkApplications) DeleteCollection(options *v1.DeleteOptions, listOptions v1.ListOptions) error {
+func (c *FakeFlinkApplications) DeleteCollection(ctx context.Context, options *v1.DeleteOptions, listOptions v1.ListOptions) error {
 	action := testing.NewDeleteCollectionAction(flinkapplicationsResource, c.ns, listOptions)
 
 	_, err := c.Fake.Invokes(action, &v1beta1.FlinkApplicationList{})
@@ -101,7 +102,7 @@ func (c *FakeFlinkApplications) DeleteCollection(options *v1.DeleteOptions, list
 }
 
 // Patch applies the patch and returns the patched flinkApplication.
-func (c *FakeFlinkApplications) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1beta1.FlinkApplication, err error) {
+func (c *FakeFlinkApplications) Patch(ctx context.Context, name string, pt types.PatchType, data []byte, subresources ...string) (result *v1beta1.FlinkApplication, err error) {
 	obj, err := c.Fake.
 		Invokes(testing.NewPatchSubresourceAction(flinkapplicationsResource, c.ns, name, pt, data, subresources...), &v1beta1.FlinkApplication{})
 
